6791e97d3b0c38dbf2b95ce9a76afef29c4fed18
SpoilerRules <spoilerrulesowo@gmail.com>
2023-09-27 18:04:00 +0200

    StringTranslate.kt completely kotlinized

--- src/main/java/net/minecraft/client/ClientBrandEnum.kt
+++ src/main/java/net/minecraft/client/ClientBrandEnum.kt
@@ -3,5 +3,5 @@ package net.minecraft.client
 object ClientBrandEnum {
     const val WINDOW_DISPLAY_TITLE: String = "Evanescent | Minecraft 1.8.9"
     const val CLIENT_MOD_NAME: String = "vanilla"
-    const val MINECRAFT_VERSION = "Minecraft 1.8.9"
+    const val MINECRAFT_VERSION: String = "Minecraft 1.8.9"
 }
