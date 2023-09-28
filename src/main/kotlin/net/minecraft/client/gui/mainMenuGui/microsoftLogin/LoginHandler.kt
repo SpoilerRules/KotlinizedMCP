591e0cd060028d8d464a81c44080279d08761c8a
SpoilerRules <spoilerrulesowo@gmail.com>
2023-09-28 20:13:27 +0200

    LoginHandler.kt crash fix

--- src/main/kotlin/net/minecraft/client/gui/mainMenuGui/microsoftLogin/LoginHandler.kt
+++ src/main/kotlin/net/minecraft/client/gui/mainMenuGui/microsoftLogin/LoginHandler.kt
@@ -2,6 +2,7 @@ package net.minecraft.client.gui.mainMenuGui.microsoftLogin
 
 import com.mojang.authlib.Agent
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService
+import fr.litarvan.openauth.microsoft.MicrosoftAuthResult
 import fr.litarvan.openauth.microsoft.MicrosoftAuthenticator
 import net.minecraft.util.Session
 import org.apache.logging.log4j.LogManager
@@ -9,19 +10,22 @@ import java.net.Proxy
 
 class LoginHandler {
     private val microsoftAuthenticator = MicrosoftAuthenticator()
+    private val authResult = microsoftAuthenticator.loginWithWebview()
     private val log = LogManager.getLogger()
 
-    fun initiateLogin(): Session {
-        val authResult = microsoftAuthenticator.loginWithWebview()
-        val minecraftProfile = authResult.profile
-        val userSession = Session(minecraftProfile.name, minecraftProfile.id, authResult.accessToken, "microsoft")
-        val userAuthentication = YggdrasilAuthenticationService(Proxy.NO_PROXY, "").createUserAuthentication(Agent.MINECRAFT)
+    fun initiateLogin() = authResult?.let { setSession(it) }
+
+    private fun setSession(authenticationResult: MicrosoftAuthResult): Session {
+        val minecraftProfile = authenticationResult.profile
+        val userSession = Session(minecraftProfile.name, minecraftProfile.id, authenticationResult.accessToken, "microsoft")
+        val userAuthentication =
+            YggdrasilAuthenticationService(Proxy.NO_PROXY, "").createUserAuthentication(Agent.MINECRAFT)
 
         userAuthentication.setUsername(minecraftProfile.name)
 
         log.info("Profile Name: ${minecraftProfile.name}")
         log.info("Profile ID: ${minecraftProfile.id}")
-        log.info("Access Token: ${authResult.accessToken}")
+        log.info("Access Token: ${authenticationResult.accessToken}")
         log.info("User Session created: $userSession")
         log.info("User Authentication Profile: $userAuthentication")
         log.info("Successful Login")
